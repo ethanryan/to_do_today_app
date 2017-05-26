e3af848324c2aa7a1bd924e15b211f5d2e72a31e
ethanryan <ethanryan@gmail.com>
2017-05-25 22:37:55 -0400

    input field clears onSubmit

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -9,8 +9,6 @@ class ListContainer extends Component {
     this.state = {
       task: []
     }
-    // this.handleInputChange = this.handleInputChange.bind(this)
-    // this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleAddTask(taskItem) {
@@ -24,31 +22,11 @@ class ListContainer extends Component {
   render() {
     return(
       <div>
-        <InputForm onSubmit={this.handleAddTask.bind(this)}/>
+        <InputForm onSubmit={this.handleAddTask.bind(this)} />
         <ListItem taskItem={this.state.task} />
       </div>
     )
   }
 }
-
-//
-// handleSubmit(e) {
-//   e.preventDefault()
-//   this.props.onSubmit( this.state.student )
-//   this.setState({student: ''})
-//   //window.history.pushState('/students') //adding this
-// }
-//
-// render() {
-//   return (
-    // <div className='col-md-8' >
-    //   <form onSubmit={this.handleSubmit.bind(this)}>
-    //     <label>Student Name</label>
-    //     <input type='text' value={this.state.student} onChange={this.handleInputChange}/>
-    //     <input type='submit' value='Add a Student' />
-    //   </form>
-    // </div>
-//   )
-// }
 
 export default ListContainer
